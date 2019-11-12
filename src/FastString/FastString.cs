688435cedef6a2e9feac1c03e78e21066bce33c1
finphie <19237170+finphie@users.noreply.github.com>
2019-11-12 15:11:14 +0900

    IsNullOrEmptyメソッドを追加 (#9)
    * null許容参照型を有効化
    
    * IsEmptyを追加
    
    * IsNullOrEmptyを追加
    
    * 引数をReadOnlySpanに変更
    
    * コメント追加

--- src/FastString/FastString.cs
+++ src/FastString/FastString.cs
@@ -1,9 +1,20 @@
-﻿namespace FastStringUtility
+using System;
+using System.Runtime.CompilerServices;
+
+namespace FastStringUtility
 {
     /// <summary>
     /// <see cref="string"/>関連のヘルパークラス
     /// </summary>
     public static partial class FastString
     {
+        /// <summary>
+        /// 指定された文字列がnullまたは空文字列であるかどうかを示します。
+        /// </summary>
+        /// <param name="value">対象の文字列</param>
+        /// <returns>引数がnullまたは空文字列の場合は、true。それ以外の場合はfalse。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNullOrEmpty(in ReadOnlySpan<char> value)
+            => value == null || value.Length == 0;
     }
 }
